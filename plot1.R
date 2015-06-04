a17594ca50f3d2ad359ac8fcd86572bf1926fedc
actuarialvoodoo <rthomas2003@gmail.com>
2015-06-03 23:12:47 -0400

    basic working version of processdata.R + plot1.R

---
+++ plot1.R
@@ -0,0 +1,6 @@
+
+png("plot1.png",width=480,height=480,units="px")
+hist(powerdf$Global_active_power,breaks=0:15*0.5,col="red", 
+     main="Global Active Power",
+     xlab="Global Active Power (kilowatts)")
+dev.off()
--- processdata.R
+++ processdata.R
@@ -2,8 +2,20 @@ data.address<-"https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_p
 data.folder<- "data"
 zip.file<-"exdata-data-household_power_consumption.zip"
 data.file<-"household_power_consumption.txt"
+sql.query<-"SELECT * FROM powerdata WHERE Date in ('1/2/2007','2/2/2007')"
 
-powerdata<-unz(file.path(data.folder,zip.file),data.file)
-attr(powerdata,"file.format")<-list(sep=";",header=TRUE)
+setwd("/Users/rthomas/Documents/Coursera/Exploratory Data Analysis/project")
 
-mydf<-sqldf("SELECT * FROM powerdata WHERE Date in ('2007-02-01','2007-02-02')")
+if (!file.exists(data.folder)){
+     dir.create(data.folder)
+}
+
+if (!file.exists(file.path(data.folder,zip.file))){
+     download.file(data.address,file.path(data.folder,zip.file), method="curl")
+}
+
+library(sqldf)
+powerdata<-file(file.path(data.folder,data.file),open="r")
+powerdf<-sqldf(sql.query,file.format=list(header=TRUE,sep=";"))
+#mydf<-sqldf()
+close(powerdata)
