55d74f0fc9aa2180570aafbdacf069fbcd862f0d
actuarialvoodoo <rthomas2003@gmail.com>
2015-06-03 23:45:28 -0400

    updated processdata.R to combine Data and Time variables. Created plot2.R

---
+++ plot2.R
@@ -0,0 +1,6 @@
+png("plot2.png",width=480,height=480,units="px")
+plot(powerdf$readingtime,powerdf$Global_active_power,
+     type="l", 
+     xlab="",
+     ylab="Global Active Power (kilowatts)")
+dev.off()
